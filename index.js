e52c1d825ef9a81e7d3c832e6e57bb8d8ad3e305
git12nazneen <lipinazneen65@gmail.com>
2024-05-22 00:13:59 +0600

    added delete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmgdsvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,7 +47,9 @@ async function run() {
     // add cart collection
 
     app.get('/carts', async(req, res)=>{
-      const result = await cartsCollection.find().toArray()
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await cartsCollection.find(query).toArray()
       res.send(result)
     })
 
@@ -57,7 +59,12 @@ async function run() {
       res.send(result)
     })
 
-
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
